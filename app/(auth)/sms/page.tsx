1d7dc84833e3840045257a0806163d6f057a1dd0
gon2s <ghidhi81@gmail.com>
2024-09-09 16:00:01 +0900

    sms verification server action 생성

--- app/(auth)/sms/page.tsx
+++ app/(auth)/sms/page.tsx
@@ -1,28 +1,47 @@
+'use client';
+
 import React from 'react';
+import { useFormState } from 'react-dom';
+import { handleVerifyCode } from './server-action';
 import { FormButton, FormInput } from '@/components';
 
+export const initialState = {
+  authentication: false,
+  error: undefined,
+};
+
 function SMSPage() {
+  const [state, action] = useFormState(handleVerifyCode, initialState);
+
   return (
     <div className={'flex flex-col gap-10 py-8 px-6'}>
       <div className={'flex flex-col gap-2 *:font-medium'}>
         <h1 className={'text-2xl'}>SMS 로그인</h1>
         <h2>휴대폰 번호로 로그인하기</h2>
       </div>
-      <form className={'flex flex-col gap-3'} action="">
-        <FormInput
-          type={'number'}
-          placeholder={'휴대폰 번호'}
-          required
-          errors={[]}
-        />
-        <FormInput
-          type={'number'}
-          placeholder={'인증 번호'}
-          required
-          errors={[]}
-        />
+      <form className={'flex flex-col gap-3'} action={action}>
+        {state.authentication ? (
+          <FormInput
+            name={'verification-code'}
+            type={'number'}
+            placeholder={'인증 번호'}
+            required
+            maxLength={6}
+            errors={state?.error?.formErrors}
+          />
+        ) : (
+          <FormInput
+            name={'phone-number'}
+            type={'string'}
+            placeholder={'휴대폰 번호'}
+            required
+            errors={state?.error?.formErrors}
+          />
+        )}
 
-        <FormButton>인증하기</FormButton>
+        <FormButton>
+          {!state.authentication ? '인증번호 전송' : '인증하기'}{' '}
+        </FormButton>
       </form>
     </div>
   );
