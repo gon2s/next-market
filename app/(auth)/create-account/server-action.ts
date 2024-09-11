d5bb9e1bf50e8704284a0104d3b12e765bcd9bc4
gon2s <ghidhi81@gmail.com>
2024-09-11 16:25:52 +0900

    회원가입 플로우 연동

--- app/(auth)/create-account/server-action.ts
+++ app/(auth)/create-account/server-action.ts
@@ -1,37 +1,66 @@
 'use server';
 
+import bcrypt from 'bcrypt';
+import { getIronSession } from 'iron-session';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import db from '@/lib/db';
 
 const passwordReg =
   /^(?=.*[A-Za-z])(?=.*\d|.*[!@#$%^&*()_+{}[\]:;"'<>?,./])|(?=.*\d)(?=.*[A-Za-z]|.*[!@#$%^&*()_+{}[\]:;"'<>?,./])|(?=.*[!@#$%^&*()_+{}[\]:;"'<>?,./])(?=.*[A-Za-z]|.*\d).+$/;
 
+const handleCheckUserName = async (val: string) => {
+  const user = await db.user.findUnique({
+    where: { username: val },
+    select: { id: true },
+  });
+  return !user;
+};
+
+const handleCheckUserEmail = async (val: string) => {
+  const user = await db.user.findUnique({
+    where: { email: val },
+    select: { id: true },
+  });
+  return !user;
+};
+
 const formSchema = z
   .object({
-    name: z.string().refine(val => val.length >= 3 && val.length <= 10, {
-      message: '이름은 3자 이상 10자 이하로 입력해주세요.',
-    }),
+    name: z
+      .string()
+      .refine(val => val.length >= 3 && val.length <= 10, {
+        message: '이름은 3자 이상 10자 이하로 입력해주세요',
+      })
+      .refine(val => handleCheckUserName(val), {
+        message: '이미 존재하는 이름이에요',
+      }),
     email: z
       .string()
       .trim()
-      .email({ message: '이메일 형식으로 입력해 주세요.' }),
+      .email({ message: '이메일 형식으로 입력해 주세요' })
+      .refine(val => handleCheckUserEmail(val), {
+        message: '이미 존재하는 이메일이에요',
+      }),
     password: z
       .string()
       .trim()
       .regex(passwordReg, {
         message:
-          '비밀번호는 대소문자, 숫자, 특수문자 중 두 개 이상의 조합으로 이루어져야 해요.',
+          '비밀번호는 대소문자, 숫자, 특수문자 중 두 개 이상의 조합으로 이루어져야 해요',
       })
       .refine(val => val.length >= 10 && val.length <= 20, {
-        message: '비밀번호는 10자 이상 20자 이하로 입력해주세요.',
+        message: '비밀번호는 10자 이상 20자 이하로 입력해주세요',
       }),
     confirmPassword: z.string().trim(),
   })
   .refine(
     data => data.password.length > 0 && data.password === data.confirmPassword,
-    { message: '비밀번호가 일치하지 않아요.', path: ['confirmPassword'] },
+    { message: '비밀번호가 일치하지 않아요', path: ['confirmPassword'] },
   );
 
-export const handleCreateAccount = (
+export const handleCreateAccount = async (
   _: unknown,
   formData: FormData,
   // eslint-disable-next-line consistent-return
@@ -43,8 +72,30 @@ export const handleCreateAccount = (
     confirmPassword: formData.get('confirmPassword'),
   };
 
-  const res = formSchema.safeParse(data);
+  const res = await formSchema.safeParseAsync(data);
   if (!res.success) {
     return res.error.flatten();
   }
+  const hashedPassword = await bcrypt.hash(res.data.password, 12);
+
+  const user = await db.user.create({
+    data: {
+      username: res.data.name,
+      email: res.data.email,
+      password: hashedPassword,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  const cookie = await getIronSession(cookies(), {
+    cookieName: 'gon-market',
+    password: process.env.COOKIE_PASSWORD!,
+  });
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  cookie.id = user.id;
+  await cookie.save();
+  redirect('/profile');
 };
