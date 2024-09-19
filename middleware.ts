a8d8e324ee841655ef067759e41c7a7c2b4deb96
gon2s <ghidhi81@gmail.com>
2024-09-19 18:56:51 +0900

    middleware 설정

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest } from 'next/server';
+import getSession from './lib/getSession';
+
+interface RouteUrl {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrl: RouteUrl = {
+  '/': true,
+  '/login': true,
+  '/create-account': true,
+  '/sms': true,
+};
+
+export const middleware = async (req: NextRequest) => {
+  const session = await getSession();
+  const exits = publicOnlyUrl[req.nextUrl.pathname];
+  if (!session?.id) {
+    if (!exits) {
+      return Response.redirect(new URL('/', req.url));
+    }
+  } else if (exits) {
+    return Response.redirect(new URL('/profile', req.url));
+  }
+  return null;
+};
