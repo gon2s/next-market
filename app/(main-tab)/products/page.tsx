01a87001f5ac2fa8d8b3eb90e143e56f7a1c4bb0
gon2s <ghidhi81@gmail.com>
2024-09-23 15:47:39 +0900

    상품 리스트 컴포넌트 생성

--- app/(main-tab)/products/page.tsx
+++ app/(main-tab)/products/page.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { MainProductItem } from '@/components/@Products';
+import db from '@/lib/db';
 
 const getProduct = async () => {
-  await new Promise(res => {
-    setTimeout(res, 10000);
+  const product = await db.product.findMany({
+    select: {
+      id: true,
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+    },
   });
+  return product;
 };
 
 async function ProductsPage() {
   const products = await getProduct();
-  return <div>ProductsPage</div>;
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      {products.map(li => {
+        return <MainProductItem key={li.id} {...li} />;
+      })}
+    </div>
+  );
 }
 
 export default ProductsPage;
---
+++ components/@Products/index.ts
@@ -0,0 +1,1 @@
+export { default as MainProductItem } from './MainProductItem';
